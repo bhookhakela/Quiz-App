e5440051c3c48df074ae8d99a46db765e6e2ac8a
harsh_dadwal <harshdadymon@gmail.com>
2023-08-02 21:11:57 +0530

    result screen little formatting

--- lib/Result_Screen.dart
+++ lib/Result_Screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:quiz_app/data/globals.dart';
+
 import 'package:quiz_app/result_widgets.dart';
 
 class Result extends StatelessWidget {
@@ -10,10 +12,41 @@ class Result extends StatelessWidget {
     return Container(
       width: double.infinity,
       height: double.infinity,
-      child: const Padding(
+      child: Padding(
         padding: EdgeInsets.fromLTRB(40, 50, 40, 50),
         child: SingleChildScrollView(
-          child: ResultWidget(),
+          child: Column(
+            children: [
+              RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  style: GoogleFonts.kanit(
+                    textStyle: const TextStyle(
+                      color: Color.fromARGB(255, 162, 102, 235),
+                      fontSize: 17,
+                    ),
+                  ),
+                  children: <TextSpan>[
+                    const TextSpan(
+                      text: "You got",
+                    ),
+                    TextSpan(
+                      text: " $score ",
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 26,
+                        color: Color.fromARGB(255, 193, 140, 236),
+                      ),
+                    ),
+                    const TextSpan(
+                      text: "answers correct out of 6 questions",
+                    ),
+                  ],
+                ),
+              ),
+              ResultWidget(),
+            ],
+          ),
         ),
       ),
     );
--- lib/result_widgets.dart
+++ lib/result_widgets.dart
@@ -7,36 +7,16 @@ class ResultWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        RichText(
-          textAlign: TextAlign.center,
-          text: TextSpan(
-            style: GoogleFonts.kanit(
-              textStyle: const TextStyle(
-                color: Color.fromARGB(255, 162, 102, 235),
-                fontSize: 17,
-              ),
-            ),
-            children: <TextSpan>[
-              const TextSpan(
-                text: "You got",
-              ),
-              TextSpan(
-                text: " $score ",
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 26,
-                  color: Color.fromARGB(255, 193, 140, 236),
-                ),
-              ),
-              const TextSpan(
-                text: "answers correct out of 6 questions",
-              ),
-            ],
-          ),
-        ),
-      ],
+    return Center(
+      child: Row(
+        children: [
+          FloatingActionButton.small(
+            child: Icon(Icons.abc),
+            onPressed: () {},
+            backgroundColor: Colors.amber,
+          )
+        ],
+      ),
     );
   }
 }
